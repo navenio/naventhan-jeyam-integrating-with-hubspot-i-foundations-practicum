32cda355c12f9d7f410187628df2ec2798da2255
navenio <naventhan@pinetco.com>
2025-12-02 16:06:35 +0530

    Set Index.js file

--- index.js
+++ index.js
@@ -1,71 +1,72 @@
-const express = require('express');
-const axios = require('axios');
+require("dotenv").config();
+
+const express = require("express");
+const axios = require("axios");
+const path = require("path");
 const app = express();
 
-app.set('view engine', 'pug');
-app.use(express.static(__dirname + '/public'));
+const PORT = process.env.PORT || 3000;
+const HUBSPOT_TOKEN = process.env.HUBSPOT_ACCESS_TOKEN;
+const OBJECT_TYPE_ID = process.env.CUSTOM_OBJECT_TYPE_ID;
+
+app.set("view engine", "pug");
+app.set("views", path.join(__dirname, "views"));
 app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+app.use(express.static(path.join(__dirname, "public")));
 
-// * Please DO NOT INCLUDE the private app access token in your repo. Don't do this practicum in your normal account.
-const PRIVATE_APP_ACCESS = '';
-
-// TODO: ROUTE 1 - Create a new app.get route for the homepage to call your custom object data. Pass this data along to the front-end and create a new pug template in the views folder.
-
-// * Code for Route 1 goes here
-
-// TODO: ROUTE 2 - Create a new app.get route for the form to create or update new custom object data. Send this data along in the next route.
-
-// * Code for Route 2 goes here
-
-// TODO: ROUTE 3 - Create a new app.post route for the custom objects form to create or update your custom object data. Once executed, redirect the user to the homepage.
-
-// * Code for Route 3 goes here
-
-/** 
-* * This is sample code to give you a reference for how you should structure your calls. 
-
-* * App.get sample
-app.get('/contacts', async (req, res) => {
-    const contacts = 'https://api.hubspot.com/crm/v3/objects/contacts';
-    const headers = {
-        Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
-        'Content-Type': 'application/json'
-    }
-    try {
-        const resp = await axios.get(contacts, { headers });
-        const data = resp.data.results;
-        res.render('contacts', { title: 'Contacts | HubSpot APIs', data });      
-    } catch (error) {
-        console.error(error);
-    }
+const hs = axios.create({
+  baseURL: "https://api.hubapi.com",
+  headers: {
+    Authorization: `Bearer ${HUBSPOT_TOKEN}`,
+    "Content-Type": "application/json",
+  },
 });
 
-* * App.post sample
-app.post('/update', async (req, res) => {
-    const update = {
-        properties: {
-            "favorite_book": req.body.newVal
-        }
-    }
+app.get("/", async (req, res) => {
+  try {
+    const properties = ["name_of_the_course", "course_owner", "course_id", "price"];
+    const limit = 20;
 
-    const email = req.query.email;
-    const updateContact = `https://api.hubapi.com/crm/v3/objects/contacts/${email}?idProperty=email`;
-    const headers = {
-        Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
-        'Content-Type': 'application/json'
-    };
+    const { data } = await hs.post(`/crm/v3/objects/${OBJECT_TYPE_ID}/search`, {
+      properties,
+      limit,
+      sorts: [{ propertyName: "hs_createdate", direction: "DESCENDING" }],
+    });
 
-    try { 
-        await axios.patch(updateContact, update, { headers } );
-        res.redirect('back');
-    } catch(err) {
-        console.error(err);
-    }
-
+    const rows = (data.results || []).map((r) => ({
+      id: r.id,
+      ...r.properties,
+    }));
+    res.render("homepage", { title: "Courses", rows });
+  } catch (err) {
+    console.error(err?.response?.data || err.message);
+    res.status(500).send("Failed to load records");
+  }
 });
-*/
 
+app.get("/update-cobj", async (req, res) => {
+  res.render("updates", {
+    title: "Update Course",
+  });
+});
 
-// * Localhost
-app.listen(3000, () => console.log('Listening on http://localhost:3000'));
+app.post("/update-cobj", async (req, res) => {
+  try {
+    const { name_of_the_course, course_owner, course_id, price } = req.body;
+
+    await hs.post(`crm/v3/objects/${OBJECT_TYPE_ID}`, {
+      properties: {
+        name_of_the_course,
+        price,
+        course_owner,
+        course_id,
+      },
+    });
+    res.redirect("/");
+  } catch (err) {
+    console.error(err?.response?.data || err.message);
+    res.status(500).send("Failed to Create Record");
+  }
+});
+
+app.listen(3000, () => console.log(`http://localhost:${PORT}`));
